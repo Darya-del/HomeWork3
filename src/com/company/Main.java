cf81e585749003eaf09cff31ad6c8068e0fb45b2
daryatraxel <darya.traxel@gmx.de>
2021-04-12 13:36:50 +0200

    исправлено

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -60,9 +60,7 @@ public class Main {
         array[8] = 9;
         array[9] = 10;
 
-        for (int i = 0;
-             i<array.length ;
-             i++){
+        for (int i = array.length-1; i>= 0 ; i--){
             System.out.println(array[i] + " ");
         }
 
@@ -75,9 +73,7 @@ public class Main {
         squareArray[1][2] = 6;
 
 
-        for (int i = 0;
-             i<squareArray.length ;
-             i++) {
+        for (int i = 0; i<squareArray.length ; i++) {
             for (int j = 0; j < squareArray[i].length; j++) {
                 System.out.print(squareArray[i][j] +"\t" +"["+i+"]"  +"["+j+"]"+" ");
             }
